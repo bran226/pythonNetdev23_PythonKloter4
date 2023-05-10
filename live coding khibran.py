dafd776e2e95ae97c82eee818f79d9bb084d441e
KhibranAkbar <126368956+bran226@users.noreply.github.com>
2023-05-10 20:30:34 +0700

    Add files via upload

---
+++ live coding khibran.py
@@ -0,0 +1,12 @@
+nomor_satu = float(input("Masukkan nomor 1: "))
+nomor_dua = float(input("Masukkan nomor 2: "))
+nomor_tiga = float(input("Masukkan nomor 3: "))
+
+if nomor_satu > nomor_dua and nomor_tiga:
+    print( f{nomor_satu}, "adalah nilai maksimal")
+    
+elif nomor_dua > nomor_satu and nomor_tiga:
+    print({nomor_dua}, "adalah nilai maksimal")
+    
+elif nomor_tiga > nomor_satu and nomor_dua:
+    print({nomor_tiga}, "adalah nilai maksimal")
